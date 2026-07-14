441b04842aa1e28a5090f061a8362d699e48ef96
Udayvir Singh Rai <udayvir2002@gmail.com>
2026-07-14 13:50:02 +0530

    fix: expose module app

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def _seed_users():
         db.session.add_all([alice, bob])
         db.session.commit()
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
